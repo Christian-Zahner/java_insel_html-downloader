0e3ff4b24cc0871f1610df782d57345cd5ef1bb4
Daniel Hentzschel <5008270+DHentzschel@users.noreply.github.com>
2018-09-27 02:12:08 +0200

    Fixed version.

--- JavaInselDownload/main.cpp
+++ JavaInselDownload/main.cpp
@@ -8,7 +8,7 @@ int main(int argc, char** argv)
 {
     QCoreApplication a(argc, argv);
     
-    std::cout << "Java ist auch eine Insel HTML downloader - 1.0.0.0";
+    std::cout << "Java ist auch eine Insel HTML downloader - 1.0.0\n";
     Downloader downloader;
     downloader.executeDownload();
 
